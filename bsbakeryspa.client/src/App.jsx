a70958d908063a8528c24b6246ac74f9c426c91b
RobotReactor <6305446+RobotReactor@users.noreply.github.com>
2025-03-24 13:06:27 -0400

    Create navbar scroll responsiveness

--- bsbakeryspa.client/src/App.jsx
+++ bsbakeryspa.client/src/App.jsx
@@ -5,12 +5,29 @@ import logo from '../src/assets/logo.jpg';
 
 
 const App = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div>
-            <nav className="navbar">
+            <nav className={`navbar ${isScrolled ? 'navbar-scrolled' : ''}`}>
                 <div className="navbar-left">
-                    <img src={logo} alt="B's Bakery Logo" className="logo" />
-                    <ul className="nav-menu">
+                    <img src={logo} alt="B's Bakery Logo" className={`logo ${isScrolled ? 'logo-scrolled' : ''}`} />
+                    <ul className={`nav-menu ${isScrolled ? 'nav-menu-scrolled' : ''}` }>
                         <li><a href="#menu">Menu</a></li>
                         <li><a href="#order">Order</a></li>
                         <li><a href="#contact">Contact</a></li>
@@ -22,6 +39,9 @@ const App = () => {
                     <FaShoppingCart className="icon" id="cartIcon" />
                 </div>
             </nav>
+            <div className="content">
+                {/* Your content here */}
+            </div>
         </div>
     );
 };
