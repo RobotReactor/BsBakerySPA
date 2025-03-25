527f5f13e6cc84638c2a6f7f16da06c8afed95ac
RobotReactor <6305446+RobotReactor@users.noreply.github.com>
2025-03-25 14:29:36 -0400

    Add menu to page and adjust item spacing

--- bsbakeryspa.client/src/App.jsx
+++ bsbakeryspa.client/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState, React } from 'react';
 import './App.css';
-import { FaUser, FaShoppingCart } from 'react-icons/fa';
+import { FaUser, FaShoppingCart, FaArrowDown } from 'react-icons/fa';
 import logo from '../src/assets/logo.jpg';
+import bagels from '../src/assets/Bagels.jpg';
+import loafs from '../src/assets/Loafs.jpg';
+import cookies from '../src/assets/Cookies.jpg';
 import baked_goods from '../src/assets/baked_goods.jpg';
 
 
@@ -43,16 +46,55 @@ const App = () => {
             </nav>
             <section className="home" id="home">
                 <div className="homeSpacer"></div>
+                <div className="heroContainer">
                     <div className="heroTitle">
                         <h1 className="heroText">Welcome to B's Bakery!</h1>
                         <h2 className="heroText">Fresh from the oven - Homemade Sourdough</h2>
                     </div>
-                <img className="bakedGoods" alt="Baked Goods Image" src={baked_goods} />
-                <div className="heroText"><p>Explore my menu and order to experience the taste of Sourdough goodness!</p></div>
+                    <img className="bakedGoods" alt="Baked Goods Image" src={baked_goods} />
+                </div>
+                <div className="heroText" id="homeBottomText"><p>Explore my menu and order to experience the taste of Sourdough goodness!</p><FaArrowDown id="arrowDownIcon" /></div>
             </section>
-            <div className="content">
-                {/* Your content here */}
-            </div>
+            <section className="menu" id="menu">
+                <div className="menu-item">
+                    <img src={bagels} alt="Bagels" className="menu-image" />
+                    <div className="menu-text">
+                        <h2>BAGELS</h2>
+                        <p>$22 / dozen | $12 / half-dozen (plain)</p>
+                        <p>+ $2 per topping (4 ea. topping - max 3 types)</p>
+                        <ul>
+                            <li>Cheddar</li>
+                            <li>Asiago</li>
+                            <li>Sesame</li>
+                            <li>Everything</li>
+                            <li>Cheddar Jalape&ntilde;o</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <img src={loafs} alt="Loafs" className="menu-image" />
+                    <div className="menu-text">
+                        <h2>LOAFS</h2>
+                        <p>$12 Regular (2 for $20) | $16 Inclusions</p>
+                        <ul>
+                            <li>Pepperoni Mozzarella</li>
+                            <li>Cheddar Jalape&ntilde;o</li>
+                            <li>Cinnamon Apple</li>
+                            <li>Everything</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <img src={cookies} alt="Cookies" className="menu-image" />
+                    <div className="menu-text">
+                        <h2>COOKIES</h2>
+                        <p>$20 / dozen</p>
+                        <ul>
+                            <li>Chocolate Chip</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
